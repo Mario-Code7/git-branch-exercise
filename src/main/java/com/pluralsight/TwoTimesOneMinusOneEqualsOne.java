d646884f501f1ae0f5d58a87d9ce85872cf20829
Mario-Code7 <marioroa71@yahoo.com>
2025-11-26 11:00:30 -0600

    initial commit

---
+++ src/main/java/com/pluralsight/TwoTimesOneMinusOneEqualsOne.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class TwoTimesOneMinusOneEqualsOne {
+    public static void main(String[] args) {
+        final Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("A little wuuuuu yeahh");
+        myScanner.nextLine();
+
+    }
+}
